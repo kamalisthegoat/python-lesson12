fe0226003688cfff8107ce9f5643e4d2b68a515c
kamalisthegoat <manjusundaram1983@gmail.com>
2026-05-03 15:19:59 +0530

    Add files via upload

---
+++ acp1.py
@@ -0,0 +1,18 @@
+print("half pyramid pattern of star (*)")
+n = int(input("enter the number of rows: "))
+
+for i in range(n):
+
+    for j in range(i+1):
+        print("*", end="") 
+
+    for j in range(n-i-1):
+        print(" ", end="")      
+
+    for j in range(n-i-1):
+        print(" ", end="")       
+    
+    for j in range(i+1):
+        print("*", end="")      
+        
+    print()
