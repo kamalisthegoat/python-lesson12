2bea5c262b44fa563404894ac5136bf0c60366a2
kamalisthegoat <manjusundaram1983@gmail.com>
2026-05-31 15:18:10 +0530

    Add files via upload

--- acp.py
+++ acp.py
@@ -1,4 +1,22 @@
-import turtle
-for i in range(4):
-    turtle.forward(120)
-    turtle.right(90)
+jj = int(input("enter the first number of the range :"))
+jj2 = int(input("enter the ending number of the range :"))
+
+sqr = []
+
+for i in range(jj, jj2+1):
+    sqr.append(i*i)
+print("the sqaures of the numbers are,", sqr)
+
+evens= []
+odd = []
+
+for j in sqr:
+    if j % 2 == 0:
+        evens.append(j)
+
+    else:
+        odd.append(j)    
+
+print("the even square are :", evens)
+
+print("the odd square are :", odd)
