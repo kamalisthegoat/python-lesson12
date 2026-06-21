aad09efc9a5d9ec323d92d45ae9ce449536b9b5a
kamalisthegoat <manjusundaram1983@gmail.com>
2026-06-21 15:10:13 +0530

    Add files via upload

--- acp.py
+++ acp.py
@@ -1,22 +1,12 @@
-jj = int(input("enter the first number of the range :"))
-jj2 = int(input("enter the ending number of the range :"))
+n = int(input("eenter a number: "))
 
-sqr = []
+odd = [i for i in range(n) if i % 2 != 0]
+even = [i for i in range(n) if i % 2 == 0]
 
-for i in range(jj, jj2+1):
-    sqr.append(i*i)
-print("the sqaures of the numbers are,", sqr)
+print("odd numbers area:", odd)
+print("even numbers are:", even)
 
-evens= []
-odd = []
+fruity = ["apple", "banana", "mango", "orange", "grapes"]
+fruitybrother = [fruit.capitalize() for fruit in fruity]
 
-for j in sqr:
-    if j % 2 == 0:
-        evens.append(j)
-
-    else:
-        odd.append(j)    
-
-print("the even square are :", evens)
-
-print("the odd square are :", odd)
+print("capitalized of the fruits is:", fruitybrother)
